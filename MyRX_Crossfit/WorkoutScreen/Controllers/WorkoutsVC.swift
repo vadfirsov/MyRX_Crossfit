d68cc59f6a5fd8ff9a6d91b47074b193737cc820
Vadim Firsov <4firsov@gmail.com>
2019-10-06 09:58:38 +0300

    Initial workout_screen commit

---
+++ MyRX_Crossfit/WorkoutScreen/Controllers/WorkoutsVC.swift
@@ -0,0 +1,9 @@
+//
+//  WorkoutsVC.swift
+//  MyRX_Crossfit
+//
+//  Created by VADIM FIRSOV on 06/10/2019.
+//  Copyright © 2019 VADIM FIRSOV. All rights reserved.
+//
+
+import Foundation
---
+++ MyRX_Crossfit/WorkoutScreen/CustomViews/WorkoutCell.swift
@@ -0,0 +1,24 @@
+//
+//  WorkoutCell.swift
+//  MyRX_Crossfit
+//
+//  Created by VADIM FIRSOV on 06/10/2019.
+//  Copyright © 2019 VADIM FIRSOV. All rights reserved.
+//
+
+import UIKit
+
+class WorkoutCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
