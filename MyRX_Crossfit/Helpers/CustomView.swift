1f2943ca59546a01f67711d1e1902cc237d203ea
Vadim Firsov <4firsov@gmail.com>
2019-10-10 15:14:31 +0300

    a

---
+++ MyRX_Crossfit/Helpers/CustomView.swift
@@ -0,0 +1,21 @@
+//
+//  CustomView.swift
+//  MyRX_Crossfit
+//
+//  Created by VADIM FIRSOV on 09/10/2019.
+//  Copyright © 2019 VADIM FIRSOV. All rights reserved.
+//
+
+import UIKit
+
+class CustomView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ MyRX_Crossfit/WorkoutScreen/Controllers/WorkoutDetailsVC.swift
@@ -0,0 +1,30 @@
+//
+//  WorkoutDetailsVC.swift
+//  MyRX_Crossfit
+//
+//  Created by VADIM FIRSOV on 08/10/2019.
+//  Copyright © 2019 VADIM FIRSOV. All rights reserved.
+//
+
+import UIKit
+
+class WorkoutDetailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
