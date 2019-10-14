12c6e14fa0e9533490e7d1fd2ced17c40f81720c
Vadim Firsov <4firsov@gmail.com>
2019-10-14 13:38:09 +0300

    Working on MainVC

---
+++ MyRX_Crossfit/MainScreen/MainVC.swift
@@ -0,0 +1,9 @@
+//
+//  MainVC.swift
+//  MyRX_Crossfit
+//
+//  Created by VADIM FIRSOV on 13/10/2019.
+//  Copyright © 2019 VADIM FIRSOV. All rights reserved.
+//
+
+import Foundation
---
+++ MyRX_Crossfit/SettingsScreen/SettingsVC.swift
@@ -0,0 +1,24 @@
+//
+//  SettingsVC.swift
+//  MyRX_Crossfit
+//
+//  Created by VADIM FIRSOV on 11/10/2019.
+//  Copyright © 2019 VADIM FIRSOV. All rights reserved.
+//
+
+import UIKit
+
+class SettingsVC : UITableViewController {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 5
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        return UITableViewCell()
+//    }
+    
+//    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+}
